af07941d7d18c3f3845841b24b44d6138c005f30
Samuel Gime <P2694792@my365.dmu.ac.uk>
2024-05-07 16:21:55 +0100

    Created my testing enviorment And am struggling

---
+++ Testing4/tstProducts.cs
@@ -0,0 +1,17 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing4
+{
+    [TestClass]
+    public class tstProducts
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsProducts AnProduct = new clsProducts();
+            Assert.IsNotNull(AnProduct);
+        }
+    }
+}
