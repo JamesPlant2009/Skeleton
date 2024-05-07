3fd63ef02341a2fc4a7aa71f48b9c73085b48b65
Aidan395 <P2533514@my365.dmu.ac.uk>
2024-05-07 16:47:09 +0100

    Creating tests for the order table

--- Testing2/tstOrder.cs
+++ Testing2/tstOrder.cs
@@ -13,5 +13,53 @@ namespace Testing2
             clsOrder AnOrder = new clsOrder();
             Assert.IsNotNull(AnOrder);
         }
+        [TestMethod]
+        public void OrderShippedPropertyOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            Boolean testData = true;
+            AnOrder.OrderShipped = testData;
+            Assert.AreEqual(AnOrder.OrderShipped, testData);
+        }
+        [TestMethod]
+        public void OrderDatePropertyOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            DateTime testData = DateTime.Now.Date();
+            AnOrder.DateAdded = testData;
+            Assert.AreEqual(AnOrder.DateAdded, testData);
+        }
+        [TestMethod]
+        public void OrderIdPropertyOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            Int32 testData = 1;
+            AnOrder.OrderId = testData;
+            Assert.AreEqual(AnOrder.OrderId, testData);
+        }
+        [TestMethod]
+        public void OrderPricePropertyOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            Int32 testData = 1;
+            AnOrder.Price = testData;
+            Assert.AreEqual(AnOrder.Price, testData);
+        }
+        [TestMethod]
+        public void StockIdPropertyOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            string testData = "";
+            AnOrder.StockId = testData;
+            Assert.AreEqual(AnOrder.StockId, testData);
+        }
+        [TestMethod]
+        public void CustomerIdPropertyOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            Int32 testData = 1;
+            AnOrder.CustomerId = testData;
+            Assert.AreEqual(AnOrder.CustomerId, testData);
+        }
     }
 }
