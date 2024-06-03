91be40fec3a5f7ede02bbac682d1ed91afd55745
Aidan395 <P2533514@my365.dmu.ac.uk>
2024-06-03 13:07:20 +0100

    Started Lab 12, About 50% done

---
+++ ClassLibrary/clsOrderCollection.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+    public class clsOrderCollection
+    {
+        public clsOrderCollection()
+        {
+            Int32 Index = 0;
+            Int32 recordCount = 0;
+            clsDataConnection DB = new clsDataConnection();
+            DB.Execute("sproc_tblOrder_SelectAll");
+            recordCount = DB.Count;
+            while(Index < recordCount)
+            {
+                clsOrder AnOrder = new clsOrder();
+                AnOrder.OrderId = Convert.ToInt32(DB.DataTable.Rows[Index]["order_Id"]);
+                AnOrder.CustomerId = Convert.ToInt32(DB.DataTable.Rows[Index]["customer_Id"]);
+                AnOrder.Price = Convert.ToInt32(DB.DataTable.Rows[Index]["price"]);
+                AnOrder.StockId = Convert.ToString(DB.DataTable.Rows[Index]["stock_Id"]);
+                AnOrder.OrderShipped = Convert.ToBoolean(DB.DataTable.Rows[Index]["order_shipped"]);
+                AnOrder.DateOrdered = Convert.ToDateTime(DB.DataTable.Rows[Index]["order_date"]);
+                mOrderList.Add( AnOrder );
+                Index++;
+            }
+        }
+        List<clsOrder> mOrderList = new List<clsOrder>();     
+
+        public List<clsOrder> OrderList 
+        {
+            get
+            {
+                return mOrderList;
+            }
+            set
+            {
+                mOrderList = value;
+            }
+        }
+        public int Count 
+        {
+            get
+            {
+                return mOrderList.Count;
+            }
+            set
+            {
+            }
+        }
+        public clsOrder ThisOrder { get; set; }
+    }
+}
---
+++ Testing2/tstOrderCollection.cs
@@ -0,0 +1,66 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace Testing2
+{
+    [TestClass]
+    public class tstOrderCollection
+    {
+        [TestMethod]
+        public void InstacnceOK()
+        {
+            clsOrderCollection AllOrder = new clsOrderCollection();
+            Assert.IsNotNull(AllOrder);
+        }
+        [TestMethod]
+        public void OrderListOK()
+        {
+            clsOrderCollection AllOrders = new clsOrderCollection();
+            List<clsOrder> testList = new List<clsOrder>();
+            clsOrder testItem  = new clsOrder();
+            testItem.OrderShipped = true;
+            testItem.OrderId = 1;
+            testItem.StockId = "1A";
+            testItem.CustomerId = 1;
+            testItem.Price = 1;
+            testItem.DateOrdered = DateTime.Now;
+            testList.Add(testItem);
+            AllOrders.OrderList = testList;
+            Assert.AreEqual(AllOrders.OrderList, testList);
+        }
+        [TestMethod]
+        public void ThisOrderPropertyOK()
+        {
+            clsOrderCollection AllOrder = new clsOrderCollection();
+            clsOrder testOrder = new clsOrder();
+            testOrder.OrderShipped = true;
+            testOrder.OrderId = 1;
+            testOrder.StockId = "1A";
+            testOrder.CustomerId = 1;
+            testOrder.Price = 1;
+            testOrder.DateOrdered = DateTime.Now;
+            AllOrder.ThisOrder = testOrder;
+            Assert.AreEqual(AllOrder.ThisOrder, testOrder);
+
+        }
+        [TestMethod]
+        public void ListandCount()
+        {
+            clsOrderCollection AllOrder = new clsOrderCollection();
+            List<clsOrder> testList = new List<clsOrder>();
+            clsOrder testItem = new clsOrder();
+            testItem.OrderShipped = true;
+            testItem.OrderId = 1;
+            testItem.StockId = "1A";
+            testItem.CustomerId = 1;
+            testItem.Price = 1;
+            testItem.DateOrdered = DateTime.Now;
+            testList.Add(testItem);
+            AllOrder.OrderList = testList;
+            Assert.AreEqual(AllOrder.Count, testList.Count);
+        }
+
+    }
+}
