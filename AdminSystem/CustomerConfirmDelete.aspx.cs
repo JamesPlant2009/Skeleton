9ad8ac5da09cab290e9ee77e6478c1ecef222152
Makhmood0 <168427914+Makhmood0@users.noreply.github.com>
2024-06-06 23:49:09 +0100

    Update CustomerConfirmDelete.aspx.cs

--- AdminSystem/CustomerConfirmDelete.aspx.cs
+++ AdminSystem/CustomerConfirmDelete.aspx.cs
@@ -1,14 +1,46 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 
 public partial class _1_ConfirmDelete : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (!IsPostBack)
+        {
+            // If the page is not being loaded as a result of a postback, check if a CustomerID is in the session
+            if (Session["CustomerID"] != null)
+            {
+                int customerId = (int)Session["CustomerID"];
+                lblMessage.Text = $"Are you sure you want to delete customer with ID {customerId}?";
+            }
+            else
+            {
+                Response.Redirect("CustomerList.aspx");
+            }
+        }
+    }
 
+    protected void btnYes_Click(object sender, EventArgs e)
+    {
+        if (Session["CustomerID"] != null)
+        {
+            int customerId = (int)Session["CustomerID"];
+            // Implement delete functionality here, e.g., delete from database
+            DeleteCustomer(customerId);
+            Response.Redirect("CustomerList.aspx");
+        }
+    }
+
+    protected void btnNo_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("CustomerList.aspx");
+    }
+
+    private void DeleteCustomer(int customerId)
+    {
+        // In reality, delete the customer from the database
+        // Here we just display a success message for demonstration
+        lblMessage.Text = $"Customer with ID {customerId} deleted successfully!";
     }
 }
